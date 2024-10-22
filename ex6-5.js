d8bcdc776d3555abdd184809662c06866f9102c0
ishiiairu <167832900+ishiiairu@users.noreply.github.com>
2024-10-22 09:57:54 +0900

    Update ex6-5.js

--- ex6-5.js
+++ ex6-5.js
@@ -3,10 +3,10 @@ function sayhello65() {alert('Hello, ' + who65 + '!');}
 function taro65(){who65 = '太郎';}
 function hanako65() {who65 = '花子';}
 
-var who66 = 
-function gakita()
-function taro66() {alert('太郎, ' + who66 + '!');}
-function hanako66() {alert('花子, ' + who66 + '!');}
+var who66 = 'Hello'; 
+function gakita() {alert('Hello, ' + who65 + '!');}
+function taro66() {who66 = '太郎';}
+function hanako66() {who66 = '花子';}
   
 
 
