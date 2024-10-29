9eb9d06fa81dd27e390be3c63626263f35248c84
ishiiairu <167832900+ishiiairu@users.noreply.github.com>
2024-10-29 10:08:28 +0900

    Update ex06-8.js

--- ex06-8.js
+++ ex06-8.js
@@ -2,7 +2,7 @@ function sayhello68() {alert('Hello, ' + document.getElementById(‘who68').inne
 
 function taro68() {document.getElementById(‘who68').innerHTML = '太郎';}
 
-function hanako68() {document.getElementById(‘who68').innerHTML = '花子';
+function hanako68() {document.getElementById(‘who68').innerHTML = '花子';}
 
 
 
